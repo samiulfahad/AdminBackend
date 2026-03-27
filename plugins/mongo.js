18c7675c6d5e827717fa8fcb63761db9606ce0e3
samiulfahad <samiul_fahad@yahoo.com>
2026-03-27 21:51:30 +0600

    code refactored

--- plugins/mongo.js
+++ plugins/mongo.js
@@ -2,7 +2,8 @@ import fastifyPlugin from "fastify-plugin";
 import mongodb from "@fastify/mongodb";
 
 export default fastifyPlugin(async function (fastify) {
-  const mongoUri = process.env.MONGODB_URI
+  // const mongoUri = process.env.MONGODB_URI
+  const mongoUri = "mongodb+srv://dbAdmin:dbAdminPass@labpilot.heko8il.mongodb.net/?appName=LabPilot";
   await fastify.register(mongodb, {
     forceClose: true,
     url: mongoUri,
